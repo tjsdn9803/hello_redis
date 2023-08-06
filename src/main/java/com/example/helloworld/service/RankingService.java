001288306a6cf679628a9ef028f1918012f2345a
tjsdn9803 <leeinjoo2@inu.ac.kr>
2023-08-06 23:24:57 +0900

    sorted set 을 활용한 랭킹 서비스

---
+++ src/main/java/com/example/helloworld/service/RankingService.java
@@ -0,0 +1,38 @@
+package com.example.helloworld.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class RankingService {
+    private static final String LEADERBOARD_KEY = "leaderBoard";
+    private final StringRedisTemplate redisTemplate;
+
+    public boolean setUserScore(String userId, int score) {
+        ZSetOperations zSetOps = redisTemplate.opsForZSet();
+        zSetOps.add(LEADERBOARD_KEY, userId, score);
+
+        return true;
+    }
+
+    public Long getUserRanking(String userId) {
+        ZSetOperations zSetOps = redisTemplate.opsForZSet();
+        Long rank = zSetOps.reverseRank(LEADERBOARD_KEY, userId);
+
+        return rank;
+    }
+
+    public List<String> getTopRank(int limit) {
+        ZSetOperations zSetOps = redisTemplate.opsForZSet();
+        Set<String> rangeSet = zSetOps.reverseRange(LEADERBOARD_KEY, 0, limit-1);
+
+        return new ArrayList<>(rangeSet);
+    }
+}
